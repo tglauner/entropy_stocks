ab119d8fc0c5175dcf5fa11e03001e879d3284d8
Tim Glauner <glaunertim@gmail.com>
2018-09-14 21:53:09 -0400

    Added stock pulling for msft and aapl

--- calc_entropy.py
+++ calc_entropy.py
@@ -12,6 +12,13 @@ def ent(data):
 ts = pd.Series(np.random.randint(0,100,size=100))
 print ent(ts)
 print ent(pd.Series(np.random.randint(0,100,size=1000)))
-print ent(pd.Series(np.random.randint(0,100,size=10000)))
-print ent(pd.Series(np.random.randint(0,100,size=100000)))
-print ent(pd.Series(np.random.randint(0,100,size=1000000)))
+
+import json
+import urllib2
+url_aapl = 'https://api.iextrading.com/1.0/stock/aapl/chart/5y'
+url_msft = 'https://api.iextrading.com/1.0/stock/msft/chart/5y'
+pd_aapl = pd.read_json(url_aapl)
+#print pd_apple
+#print pd_apple['close']
+print 'Entropy Apple = ', ent(pd_aapl['close'])
+print 'Entropy MSFT = ', ent(pd.read_json(url_msft)['close'])
